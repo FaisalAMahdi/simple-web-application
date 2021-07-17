d95bd916c594b612432c04a7744ed9ec843c25e7
FaisalAMahdi <=>
2021-07-17 22:51:04 +0100

    i created all the controllers for the new created model

--- classes/controller.class.php
+++ classes/controller.class.php
@@ -22,6 +22,22 @@ class Control extends Model{
     public function updateUser($firstName,$lastName,$email,$phone,$address,$userId){
         return Model::updateUser($firstName,$lastName,$email,$phone,$address,$userId);
     }
+
+    public function createPost($userId,$post){
+        return Model::createPost($userId,$post);
+    }
+
+    public function createComment($postId,$comment){
+        return Model::createComment($postId,$comment);
+    }
+
+    public function getPosts(){
+        return Model::getPosts();
+    }
+
+    public function getSinglePost($userId){
+        return Model::getSinglePost($userId);
+    }
 }
 
 // $con = new Control();
